3ddeabe45a13ea8dba765f55913acdc9eba4ed9c
Milo Todorovich <milo.todorovich@gmail.com>
2023-09-29 06:53:20 -0500

    2015 Day 5 part 2 solution - not giddy

--- src/milotodorovich/aoc_clj/2015/day5.clj
+++ src/milotodorovich/aoc_clj/2015/day5.clj
@@ -1,4 +1,4 @@
-(ns milotodorovich.aoc-clj.2015.day5 
+(ns milotodorovich.aoc-clj.2015.day5
   (:require [clojure.java.io :as io]
             [clojure.string :as str]))
 
@@ -13,7 +13,7 @@
              (reduce + 0))]
     (>= num-vowels 3)))
 
-(defn if-equal? [input]
+(defn ends-equal? [input]
   (if (= (first input) (last input))
     1
     0))
@@ -22,19 +22,50 @@
   (->> input
        (seq)
        (partition 2 1)
-       (map if-equal?)
+       (map ends-equal?)
        (reduce + 0)
        (< 0)))
 
 (def disallowed '("ab" "cd" "pq" "xy"))
 
 (defn no-disallowed? [input]
-  (not-any? (fn [pair] (str/includes? input pair))disallowed))
+  (not-any? (fn [pair] (str/includes? input pair)) disallowed))
 
 (defn part-1 [input]
   (and (three-vowels? input)
        (double-letters? input)
        (no-disallowed? input)))
+
+(defn paired-triplets? [input]
+  (->> input
+       (seq)
+       (partition 3 1)
+       (map ends-equal?)
+       (reduce + 0)
+       (< 0)))
+
+(defn two-pair-non-overlapping? [input]
+;;   (let [possible-pairs (->> input
+;;                             (seq)
+;;                             (partition 2 1))]
+    ;; (not= (count possible-pairs)
+    ;;       (count (group-by identity possible-pairs))))
+   (let [first (first input)
+         second (second input)
+         first-rest (rest input)
+         remain (rest (str/join first-rest))
+         two-pair? (str/includes? (str/join remain) (str first second))]
+     (if two-pair?
+       true
+       (if (seq first-rest)
+         (two-pair-non-overlapping? (str/join (rest input)))
+         false)
+       ) 
+    ))
+
+(defn part-2 [input]
+  (and (paired-triplets? input)
+       (two-pair-non-overlapping? input)))
 
 (defn -main [& _]
   (let [data (-> "2015/day5.txt"
@@ -46,7 +77,7 @@
                   (map part-1)
                   (filter identity)
                   (count)))
-    ;; (println (->> data
-    ;;               (map part-2)
-    ;;               (reduce + 0)))
-    ))
+    (println (->> data
+                  (map part-2)
+                  (filter identity)
+                  (count)))))
--- test/milotodorovich/aoc_clj/2015/day5_test.clj
+++ test/milotodorovich/aoc_clj/2015/day5_test.clj
@@ -1,4 +1,4 @@
-(ns milotodorovich.aoc-clj.2015.day5-test 
+(ns milotodorovich.aoc-clj.2015.day5-test
   (:require [clojure.test :refer [are deftest testing]]
             [milotodorovich.aoc-clj.2015.day5 :as day5]))
 
@@ -6,8 +6,26 @@
 (deftest day-5-part-1
   (testing "day 5 part 1"
     (are [input expected] (= expected (day5/part-1 input))
-            "ugknbfddgicrmopn" true
-            "aaa" true
-            "jchzalrnumimnmhp" false
-            "haegwjzuvuyypxyu" false
-            "dvszwmarrgswjxmb" false)))
+      "ugknbfddgicrmopn" true
+      "aaa" true
+      "jchzalrnumimnmhp" false
+      "haegwjzuvuyypxyu" false
+      "dvszwmarrgswjxmb" false)))
+
+(deftest day-5-part-2
+  (testing "day 5 part 1"
+    (are [input expected] (= expected (day5/part-2 input))
+      "qjhvhtzxzqqjkmpb" true
+      "xxyxx" true
+      "uurcxstgmygtbstg" false
+      "ieodomkazucvgmuy" false
+      "aaaayzy" true
+      "aaayzy" false
+      "jbcccjb" true
+      "jbccjb" false))
+  (testing "two pair non-overlapping"
+    (are [input expected] (= expected (day5/two-pair-non-overlapping? input))
+      "aaaa" true
+      "aaa" false
+      "jbcccjb" true
+      "jbccjb" true)))
