d0fb2a64f0f5a4b24e0378ce496a5d18fa202044
Milo Todorovich <milo.todorovich@gmail.com>
2023-09-29 07:04:44 -0500

    Simplify namespaces

--- test/milotodorovich/aoc_clj_test.clj
+++
@@ -1,7 +0,0 @@
-(ns milotodorovich.aoc-clj-test
-  (:require [clojure.test :refer :all]
-            [milotodorovich.aoc-clj :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 0))))
