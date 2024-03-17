b298911193c83cddd6fe1b30c7d775aace4a71f9
Milo Todorovich <milo.todorovich@gmail.com>
2024-03-17 17:20:26 -0500

    scripts/new update with templated methods

---
+++ src/aoc/2023/04.clj
@@ -0,0 +1,10 @@
+
+(ns aoc.2023.04)
+
+(defn part-1 [input]
+  0)
+
+(defn part-2 [input]
+  0)
+
+
---
+++ test/aoc/2023/04_test.clj
@@ -0,0 +1,14 @@
+
+(ns aoc.2023.04-test
+  (:require [aoc.2023.04 :as sut]
+            [clojure.testing :as t]))
+
+(t/deftest part-1-test
+  (t/testing hello
+    (t/is (= 1 0))))
+
+(t/deftest part-2-test
+  (t/testing world
+    (t/is (= 1 2))))
+
+
